acffc2b76aaae682fbe936f5464e7e88be69d851
Viridiana <sbelloni18@gmail.com>
2017-11-02 23:53:10 +0100

    modified:   functions.php

--- functions.php
+++ functions.php
@@ -3,19 +3,6 @@
 // File Security Check
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-/*
-function my_theme_enqueue_styles() {
-    $parent_style = 'storefront'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'tvox-child',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
-}
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-*/
 
 /*-----------------------------------------------------------------------------------*/
 /* Fact Boxes - box
